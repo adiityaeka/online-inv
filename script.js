d984579fe04b7dad01c3aa613c5e8ff52c6e590b
Aditya Eka <atg.adit.pku@gmail.com>
2025-08-12 15:01:23 +0700

    final api update

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const pagination = document.getElementById("pagination");
 const MESSAGES_PER_PAGE = 5;
 let currentPage = 1;
 
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxtJ-oeUSN36w0aeqFTgdTUkjshs1q0HkhvGnimeA-l13bjGPJ3xuaLgvc7AgJbi95nJw/exec"; // Replace with your deployed Apps Script URL
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycby0GqvsJEiwG6NdqDIxWzJbFQvNsAgVmuOcogGtuYWgJ0BwmcjExKGoBcYsdnmHWl_-yQ/exec"; // Replace with your deployed Apps Script URL
 
 async function fetchMessages() {
   try {
